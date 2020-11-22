762f462145635c0b05c15fca143cfe0611dd0869
Leon Kountouras <koundouras@gmail.com>
2020-11-22 20:29:20 +0200

    Add files via upload

--- js/afmcheck.js
+++ js/afmcheck.js
@@ -3,27 +3,19 @@
 Το υπόλοιπο του αθροίσματος με το 11 πρέπει να είναι ίσο με το τελευταίο ψηφίο του ΑΦΜ.
 (Το τελευταίο ψηφίο δεν αποτελεί παράγοντας στον υπολογισμό του αθροίσματος) */
  
-
-function afmcheck() {
-    let uservalue = null; 
+function afmcheck() { 
     let sum = 0;
-    let multiplier = 256;
-
     var userinput = $('#input_afm').val();
-    //$('#alert').html('The user input is: ' + userinput);
-    uservalue= userinput;
-
-    let afm = uservalue.split(",");
-    afm = afm.join(",")
-    for (let i=0;i<afm.length-1;i++){
-        sum += afm[i] * multiplier;
-        multiplier /= 2;  
+    let afm = userinput.split(",").join(",");
+    for (let i=0;i<afm.length-1;++i){
+        sum += afm[i] * Math.pow(2, 8-i);
     }
     var result = sum % 11;
+    console.log(afm.split("").pop())
     if((afm.length<9) || (sum == 0)){ // αν ο αριθμός είναι μικρότερος του 0 ή αν το όλα τα ψηφία είναι 0
         return false;
     }
-    else if (result == afm.split("").pop()){ // ελέγχει αν το αποτέλεσμα είναι ίσο με το τελευταίο ψηφίο του ΑΦΜ
+    else if ((result == afm.split("").pop())||((result == 0 || result == 10) && afm.split("").pop() == 0)){ // ελέγχει αν το αποτέλεσμα είναι ίσο με το τελευταίο ψηφίο του ΑΦΜ
         return true;
     }
   }
